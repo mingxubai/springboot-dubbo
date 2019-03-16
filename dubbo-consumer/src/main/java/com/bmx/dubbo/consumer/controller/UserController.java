d53c88e50ccdb7206694d76f3fb9fbfb0129e510
baimingxu <baimingxu@outllok.com>
2019-03-16 14:46:45 +0800

    整合Spring Boot + Dubbo + Redis + RabbitMQ + Mybatis + Swagger2

--- dubbo-consumer/src/main/java/com/bmx/dubbo/consumer/controller/UserController.java
+++ dubbo-consumer/src/main/java/com/bmx/dubbo/consumer/controller/UserController.java
@@ -4,6 +4,8 @@ package com.bmx.dubbo.consumer.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.bmx.api.service.UserService;
 import com.bmx.dubbo.dao.pojo.User;
+import entity.Result;
+import entity.StatusCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -23,15 +25,18 @@ public class UserController {
 
     @ApiOperation(value="获取用户列表", notes="获取用户列表")
     @RequestMapping(value = "findAll", method = RequestMethod.GET)
-    public List<User> findAll(){
-        return userService.findAll();
+    public Result findAll(){
+        List<User> list = userService.findAll();
+        return new Result(true, StatusCode.OK, "查询成功", list);
     }
 
     @ApiOperation(value="获取用户详细信息", notes="根据id来获取用户详细信息")
     @ApiImplicitParam(name = "id", value = "用户ID", dataType = "Integer", paramType = "path")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public User findUserById(@PathVariable(value = "id") Integer id) throws IOException {
+    public Result findUserById(@PathVariable(value = "id") Integer id) throws IOException {
 
-        return userService.findUserById(id);
+        User user = userService.findUserById(id);
+        return new Result(true, StatusCode.OK, "查询成功", user);
+
     }
 }
