4f33f1a68fd80440d35d57e1a48220e39b673bea
baimingxu <baimingxu@outllok.com>
2019-03-27 09:39:57 +0800

    整合Spring Boot + Dubbo + Redis + RabbitMQ + Mybatis + Swagger2

--- dubbo-dao/src/main/java/com/bmx/dubbo/dao/mapper/UserMapper.java
+++ dubbo-dao/src/main/java/com/bmx/dubbo/dao/mapper/UserMapper.java
@@ -8,13 +8,22 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 /**
- *
+ * 用户mapper接口
  */
 @Mapper
 public interface UserMapper {
 
+    /**
+     * 查询用户列表
+     * @return
+     */
     List<User> findAll();
 
-    @Select("select * from user where id = #{id}")
+    /**
+     * 根据id查询用户详情
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = ${id}")
     User findUserById(Integer id);
 }
